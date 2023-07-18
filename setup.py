484e59a9154b2f4c19d65836c630aed2ae76b404
Ramanakumar Sankar <rsankar@umn.edu>
2023-07-18 18:23:01 -0500

    fixing flake issues

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 from epic_extractor import __version__
 
-setup(name='EPIC_Extractor', 
-    version=__version__,
-    description='Python scripts to extract EPIC outputs',
-    author='Ramanakumar Sankar',
-    url='https://github.com/ramanakumars/EPIC_extractor',
-    packages=['epic_extractor'],
-    install_requires=[
-        'numpy',
-        'netCDF4',
-    ]
-)
+setup(name='EPIC_Extractor',
+      version=__version__,
+      description='Python scripts to extract EPIC outputs',
+      author='Ramanakumar Sankar',
+      url='https://github.com/ramanakumars/EPIC_extractor',
+      packages=['epic_extractor'],
+      install_requires=[
+          'numpy',
+          'netCDF4',
+      ]
+      )
